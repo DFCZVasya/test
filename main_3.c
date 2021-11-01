c6ac1f81a46ce695d79b30b6212469269cadea14
Vasya Lopatkin <48513210+DFCZVasya@users.noreply.github.com>
2021-11-01 16:16:11 +0700

    Update main_3.c

--- main_3.c
+++ main_3.c
@@ -1,20 +1,34 @@
-#include <math.h>
 #include <stdio.h>
 #include <gsl/gsl_blas.h>
+#include <gsl/gsl_matrix.h>
 
-
+float max_sum_line(gsl_matrix* A, int N)
+{
+    float max = 0;
+    float sum = 0;
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            sum += gsl_matrix_get(A, i, j);
+        }
+        if (sum > max) max = sum;
+        sum = 0;
+    }
+    return max;
+}
 
 int main()
 {
  int N, M;
  scanf("%d %d", &N, &M);
- gsl_matrix *I = gsl_matrix_calloc(N, N);
- gsl_matrix* A = gsl_matrix_calloc(N, N);
+ gsl_matrix *I = gsl_matrix_alloc(N, N);
+ gsl_matrix* A = gsl_matrix_alloc(N, N);
  for (int i = 0; i < N; i++)
  {
   for (int ii = 0; ii < N; ii++)
   {
-   gsl_matrix_set(A, i, ii, rand() % 101;);
+   gsl_matrix_set(A, i, ii, rand() % 101);
   }
  }
  for (int i = 0; i < N; i++)
@@ -27,25 +41,25 @@ int main()
    }
   }
  }
- gsl_matrix* B = gsl_matrix_calloc(N, N);
+ gsl_matrix* B = gsl_matrix_alloc(N, N);
  gsl_blas_dgemm(CblasTrans, CblasNoTrans, 1, A, I, 0, B);
- float a1 = gsl_matrix_norm1(A);
- gsl_matrix* AT = gsl_matrix_calloc(N, N);
+ float a1 = max_sum_line(A, N);
+ gsl_matrix* AT = gsl_matrix_alloc(N, N);
  gsl_blas_dgemm(CblasTrans, CblasNoTrans, 1, A, I, 0, AT);
- float aInf = gsl_matrix_norm1(AT);
- gsl_matrix* BN = gsl_matrix_calloc(N, N);
+ float aInf = max_sum_line(AT, N);
+ gsl_matrix* BN = gsl_matrix_alloc(N, N);
  gsl_blas_dgemm(CblasNoTrans, CblasNoTrans, 1/(a1 * aInf), B, I, 0, BN);
  gsl_matrix_memcpy(B, BN);
  gsl_matrix* R = gsl_matrix_alloc(N, N);
  gsl_matrix_memcpy(R, I);
  gsl_blas_dgemm(CblasNoTrans, CblasNoTrans, -1, B, A, 1, R);
- gsl_matrix* AMO = gsl_matrix_calloc(N, N);
- gsl_matrix* AMON = gsl_matrix_calloc(N, N);
- gsl_matrix* NT = gsl_matrix_calloc(N, N);
- gsl_matrix* NTN = gsl_matrix_calloc(N, N);
+ gsl_matrix* AMO = gsl_matrix_alloc(N, N);
+ gsl_matrix* AMON = gsl_matrix_alloc(N, N);
+ gsl_matrix* NT = gsl_matrix_alloc(N, N);
+ gsl_matrix* NTN = gsl_matrix_alloc(N, N);
  gsl_matrix_memcpy(AMO, I);
  gsl_matrix_memcpy(NT, R);
- for (int i = 0; i < m; i++)
+ for (int i = 0; i < M; i++)
  {
   gsl_matrix_add(AMO, NT);
   gsl_blas_dgemm(CblasNoTrans, CblasNoTrans, 1, NT, R, 0, NTN);
@@ -53,6 +67,6 @@ int main()
  }
  gsl_blas_dgemm(CblasNoTrans, CblasNoTrans, 1, AMO, B, 0, AMON);
  gsl_matrix_memcpy(AMO, AMON);
- printf("ready!\n");
+ 
  return 0;
 }
